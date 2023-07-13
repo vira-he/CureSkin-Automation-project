9f8791edf90cc2ccdb15f30e2fcc7a0c88121f10
vira-he <vira.connect@gmail.com>
2023-07-12 21:09:54 -0500

    structure added

---
+++ app/application.py
@@ -0,0 +1,19 @@
+# from pages.header import Header
+# from pages.main_page import MainPage
+# from pages.search_results import SearchResults
+# from pages.signin_page import SignInPage
+# from pages.cart_page import CartPage
+# from pages.product_page import ProductPage
+#
+#
+# class Application:
+#
+#     def __init__(self, driver):
+#         self.driver = driver
+#         self.header = Header(self.driver)
+#         self.main_page = MainPage(self.driver)
+#         self.search_results = SearchResults(self.driver)
+#         self.signin_page = SignInPage(self.driver)
+#         self.cart_page = CartPage(self.driver)
+#         self.product_page = ProductPage(self.driver)
+#
