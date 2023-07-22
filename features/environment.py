135ff3cce72d791191fea7c2a093295dc7bfe5e4
vira-he <vira.connect@gmail.com>
2023-07-21 19:39:38 -0500

    Browserstack task completed

--- features/environment.py
+++ features/environment.py
@@ -5,6 +5,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.support.wait import WebDriverWait
 from app.application import Application
 from support.logger import logger
@@ -24,29 +26,52 @@ def browser_init(context, test_name):
 
     #### HEADLESS MODE ####
 
-    chrome_options = webdriver.ChromeOptions()
-    driver_path = ChromeDriverManager().install()
-    service = Service(driver_path)
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--window-size=1920,1080")
-    context.driver = webdriver.Chrome(
-        options=chrome_options,
-        service=service
-    )
+    # chrome_options = webdriver.ChromeOptions()
+    # driver_path = ChromeDriverManager().install()
+    # service = Service(driver_path)
+    # chrome_options.add_argument('--headless')
+    # chrome_options.add_argument("--disable-gpu")
+    # chrome_options.add_argument("--window-size=1920,1080")
+    # context.driver = webdriver.Chrome(
+    #     options=chrome_options,
+    #     service=service
+    # )
 
     #### BROWSERSTACK ####
 
+    options = webdriver.FirefoxOptions()
+    options.set_preference('profile', "/Users/test/Library/Application Support/Firefox/Profiles/<name_of_your_profile>")
+
+    caps = {
+
+        "os": "Windows",
+        "osVersion": "11",
+        "buildName": "firefoxprofile- python",
+        "sessionName": "firefoxprofile- python",
+        "browserName": "Firefox",
+    }
+
+    options.set_capability('bstack:options', caps)
+
+    context.driver = webdriver.Remote(
+        command_executor='http://virahetman_JQceH1:H886nJkaPzvgr9Nkneup@hub.browserstack.com/wd/hub',
+        options=options)
+
+    # LANA"S CODE
     # desired_cap = {
-    #     'browser': 'Chrome',
-    #     'os_version': '11',
-    #     'os': 'Windows',
-    #     'name': test_name
+    #     'bstack:options': {
+    #         "os": "Windows",
+    #         "osVersion": "11",
+    #         "browserVersion": "latest",
+    #         "local": "false",
+    #         "seleniumVersion": "3.5.2",
+    #     },
+    #     "browserName": "Edge",
     # }
     # bs_user = 'virahetman_JQceH1'
     # bs_key = 'H886nJkaPzvgr9Nkneup'
     # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-    # context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
+    # context.driver = webdriver.Remote(url, options=options)
 
     context.driver.maximize_window()
 
@@ -63,6 +88,7 @@ def before_scenario(context, scenario):
     # print(f'Started scenario: ', {scenario.name})
     logger.info(f'Started scenario: {scenario.name}')
     browser_init(context, scenario.name)
+    context.driver.execute_script('browserstack_executor:{"action":"setSessionName", "arguments":{"name": " ' + scenario.name + ' " }}')
 
 
 def before_step(context, step):
