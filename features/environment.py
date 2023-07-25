014cd97e0a4a5a03f4c5155ec415f99aa46da289
vira-he <vira.connect@gmail.com>
2023-07-25 13:42:38 -0500

    allure task completed

--- features/environment.py
+++ features/environment.py
@@ -12,20 +12,17 @@ from app.application import Application
 from support.logger import logger
 
 
-def browser_init(context, test_name):
+def browser_init(context, scenario):
     """
     :param context: Behave context
     """
-    # driver_path = ChromeDriverManager().install()
-    # service = Service(driver_path)
+    # service = Service(executable_path='/Users/vera/Desktop/QAA/CureSkin-Automation-project/chromedriver')
     # context.driver = webdriver.Chrome(service=service)
 
     # context.driver = webdriver.Firefox()
     # context.driver = webdriver.Safari()
 
-
-    #### HEADLESS MODE ####
-
+    # HEADLESS MODE
     # chrome_options = webdriver.ChromeOptions()
     # driver_path = ChromeDriverManager().install()
     # service = Service(driver_path)
@@ -37,8 +34,7 @@ def browser_init(context, test_name):
     #     service=service
     # )
 
-    #### BROWSERSTACK ####
-
+    # BROWSERSTACK
     options = webdriver.FirefoxOptions()
     options.set_preference('profile', "/Users/test/Library/Application Support/Firefox/Profiles/<name_of_your_profile>")
 
@@ -50,45 +46,27 @@ def browser_init(context, test_name):
         "sessionName": "firefoxprofile- python",
         "browserName": "Firefox",
     }
-
     options.set_capability('bstack:options', caps)
 
     context.driver = webdriver.Remote(
         command_executor='http://virahetman_JQceH1:H886nJkaPzvgr9Nkneup@hub.browserstack.com/wd/hub',
         options=options)
 
-    # LANA"S CODE
-    # desired_cap = {
-    #     'bstack:options': {
-    #         "os": "Windows",
-    #         "osVersion": "11",
-    #         "browserVersion": "latest",
-    #         "local": "false",
-    #         "seleniumVersion": "3.5.2",
-    #     },
-    #     "browserName": "Edge",
-    # }
-    # bs_user = 'virahetman_JQceH1'
-    # bs_key = 'H886nJkaPzvgr9Nkneup'
-    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-    # context.driver = webdriver.Remote(url, options=options)
+    context.driver.execute_script('browserstack_executor:{"action":"setSessionName",\
+     "arguments":{"name": " ' + scenario.name + ' " }}')
 
     context.driver.maximize_window()
-
     context.driver.implicitly_wait(4)
     context.driver.wait = WebDriverWait(context.driver, 5)
-    #
     context.app = Application(context.driver)
 
     # Allure command:
-    # python3 -m behave -f allure_behave.formatter:AllureFormatter -o test_results/ features/tests/amazon_signin.feature
+    # python3 -m behave -f allure_behave.formatter:AllureFormatter -o test_results/ features/tests/cureskin_main.feature
 
 
 def before_scenario(context, scenario):
-    # print(f'Started scenario: ', {scenario.name})
     logger.info(f'Started scenario: {scenario.name}')
-    browser_init(context, scenario.name)
-    context.driver.execute_script('browserstack_executor:{"action":"setSessionName", "arguments":{"name": " ' + scenario.name + ' " }}')
+    browser_init(context, scenario)
 
 
 def before_step(context, step):
@@ -98,10 +76,7 @@ def before_step(context, step):
 
 def after_step(context, step):
     if step.status == 'failed':
-        # print('\nStep failed: ', step)
         logger.error(f'Step failed: {step}')
-        # Mark test case as FAILED on BrowserStack:
-        # Documentation: https://www.browserstack.com/docs/automate/selenium/view-test-results/mark-tests-as-pass-fail
         # context.driver.execute_script(
         #     'browserstack_executor: {"action": "setSessionStatus", "arguments": '
         #     '{"status":"failed", "reason": "Step failed"}}'
